af8bc8e3c5825b75e6e56737eaa67617d028537b
ryo4004 <kato@ryo4004.net>
2024-04-09 23:39:14 +0900

    日付を表示する

--- src/Timetable/Timetable.tsx
+++ src/Timetable/Timetable.tsx
@@ -1,3 +1,34 @@
+import { useState } from 'react'
+import { DateTime } from 'luxon'
+
 export const Timetable = () => {
-  return <>Timetable</>
+  const [startDate, setStartDate] = useState('')
+
+  return (
+    <>
+      <h2>Timetable</h2>
+      <input
+        type="date"
+        value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}
+      />
+      {startDate && (
+        <Table startDateTime={DateTime.fromFormat(startDate, 'yyyy-MM-dd')} />
+      )}
+    </>
+  )
+}
+
+const Table = ({ startDateTime }: { startDateTime: DateTime }) => {
+  const weekDaysList = [...Array(7)].map((_, add) => {
+    return startDateTime.plus({ day: add })
+  })
+
+  return (
+    <>
+      {weekDaysList.map((date) => (
+        <div key={date.toISO()}>{date.toISODate()}</div>
+      ))}
+    </>
+  )
 }
