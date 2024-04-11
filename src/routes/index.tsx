ec20d1a40afc0048b10f2c6b11c7fa0e6f80163d
ryo4004 <kato@ryo4004.net>
2024-04-11 23:58:19 +0900

    設定ページを追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Outlet, type RouteObject, useRoutes } from 'react-router-dom'
 import { Timetable } from '../features/Timetable'
+import { Settings } from '../features/settings'
 
 export const AppRoutes = () => {
   const element = useRoutes(routes)
@@ -10,6 +11,12 @@ const routes: RouteObject[] = [
   {
     path: '',
     element: <Outlet />,
-    children: [{ path: '/', element: <Timetable /> }],
+    children: [
+      { path: '/', element: <Timetable /> },
+      {
+        path: '/settings',
+        element: <Settings />,
+      },
+    ],
   },
 ]
