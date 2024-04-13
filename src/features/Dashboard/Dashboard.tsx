17d9fa13a4a94896a54fae29c8a6174245aaeb81
ryo4004 <kato@ryo4004.net>
2024-04-13 15:07:03 +0900

    時間割の作成処理を修正

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -3,19 +3,21 @@ import { useTimetableStore } from '../../stores/timetable'
 import { LoadButton } from './LoadButton'
 import { SaveButton } from './SaveButton'
 import { Link } from 'react-router-dom'
+import { useConfigStore } from '../../stores/configs'
 
 export const Dashboard = () => {
   const [startDate, setStartDate] = useState('')
+  const { timetables, createTimetable } = useTimetableStore()
+  const { config } = useConfigStore()
 
-  const { timetables } = useTimetableStore()
-
-  if (timetables.length === 0) {
-    return (
-      <>
-        時間割の初期化が必要です
-        <LoadButton />
-      </>
-    )
+  const onCreate = () => {
+    if (!startDate) {
+      return
+    }
+    if (timetables.find((timetable) => timetable.firstDate === startDate)) {
+      return alert('すでに存在します')
+    }
+    createTimetable(startDate, config.classes)
   }
 
   return (
@@ -26,6 +28,9 @@ export const Dashboard = () => {
         value={startDate}
         onChange={(e) => setStartDate(e.target.value)}
       />
+      <button onClick={onCreate} disabled={!startDate}>
+        作成
+      </button>
       <WeekTable />
       <SaveButton />
     </>
@@ -34,6 +39,15 @@ export const Dashboard = () => {
 
 const WeekTable = () => {
   const { timetables } = useTimetableStore()
+
+  if (timetables.length === 0) {
+    return (
+      <>
+        時間割の作成が必要です
+        <LoadButton />
+      </>
+    )
+  }
 
   return (
     <>
--- src/features/settings/Settings.tsx
+++ src/features/settings/Settings.tsx
@@ -1,18 +1,10 @@
-import { useTimetableStore } from '../../stores/timetable'
-import { useConfigStore } from '../../stores/configs'
 import { SubjectSettings } from './SubjectSettings'
 import { TimetableSettings } from './TimetableSettings'
 
 export const Settings = () => {
-  const { createTimetable } = useTimetableStore()
-  const { config } = useConfigStore()
-
   return (
     <>
       <h2>settings</h2>
-      <button onClick={() => createTimetable('2024-04-01', config.classes)}>
-        作成
-      </button>
       <TimetableSettings />
       <SubjectSettings />
     </>
