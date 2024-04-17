5c5815bc453e75a81e7b633052f8dcecbf8270ff
ryo4004 <kato@ryo4004.net>
2024-04-18 00:08:22 +0900

    案内ページを更新

--- src/features/Guide/Guide.tsx
+++ src/features/Guide/Guide.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom'
 import { Box } from '../../components/Layout/Box'
+import { Text } from '../../components/Layout/Text'
+import { LoadButton } from '../dashboard/components/LoadButton'
 
 export const Guide = () => {
-  return <Box>使い方</Box>
+  return (
+    <Box marginX="8px">
+      <Text as="h1" fontSize="24px" fontWeight="bold">
+        週案くん
+      </Text>
+      <LoadButton />
+      <Link to="/dashboard">ダッシュボードへ</Link>
+      <Text as="h2">使い方</Text>
+    </Box>
+  )
 }
