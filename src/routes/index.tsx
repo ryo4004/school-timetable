b1d993e526041aca34cb750d3be6c89b3722085d
ryo4004 <kato@ryo4004.net>
2024-04-20 22:35:21 +0900

    ログインページを追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Timetable } from '../features/timetable'
 import { Settings } from '../features/settings'
 import { Dashboard } from '../features/dashboard'
 import { Home } from '../features/home/Home'
+import { Login } from '../features/login'
 
 export const AppRoutes = () => {
   const element = useRoutes(routes)
@@ -16,10 +17,8 @@ const routes: RouteObject[] = [
       { path: '/', element: <Home /> },
       { path: '/dashboard', element: <Dashboard /> },
       { path: '/:key', element: <Timetable /> },
-      {
-        path: '/settings',
-        element: <Settings />,
-      },
+      { path: '/settings', element: <Settings /> },
+      { path: '/login', element: <Login /> },
     ],
   },
 ]
