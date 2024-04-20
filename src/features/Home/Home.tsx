8038c2636c77f4db35093548f920769edebde99d
ryo4004 <kato@ryo4004.net>
2024-04-20 17:45:03 +0900

    Homeを修正

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -9,9 +9,27 @@ export const Home = () => {
       <Text as="h1" fontSize="24px" fontWeight="bold">
         週案くん
       </Text>
+      <Text as="h2" marginTop="16px" fontSize="16px" fontWeight="bold">
+        保存したデータを読み込む
+      </Text>
       <LoadButton />
-      <Link to="/dashboard">ダッシュボードへ</Link>
-      <Text as="h2">使い方</Text>
+      <Text marginTop="16px" fontSize="16px" fontWeight="bold">
+        新しく開始する
+      </Text>
+      <Link to="/dashboard">
+        <Text
+          border="1px solid #ccc"
+          display="inline-block"
+          padding="2px 8px"
+          borderRadius="0.375rem"
+          _hover={{ background: '#0000000a' }}
+        >
+          ダッシュボードへ
+        </Text>
+      </Link>
+      <Text as="h2" marginTop="16px" fontSize="16px" fontWeight="bold">
+        使い方
+      </Text>
     </Box>
   )
 }
